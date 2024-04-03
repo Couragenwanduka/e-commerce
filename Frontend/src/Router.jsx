6e1e8d3dcb2438bfb0e82561fe3ad12978c2e310
couragenwanduka <courageobunike@gmail.com>
2024-04-03 23:11:29 +0100

    changed some things

--- Frontend/src/Router.jsx
+++ Frontend/src/Router.jsx
@@ -3,13 +3,15 @@ import { BrowserRouter , Routes, Route} from "react-router-dom";
 import Signup from './signup.jsx';
 import App from './App.jsx';
 import Login from './Login.jsx';
+import Lp from "./LandingPage.jsx";
 
  function Main() {
     return (
         <BrowserRouter>
         <Routes>
             <Route path="/" >
-                 <Route path="/" element={<App/>}/>
+                <Route path="/" element={<Lp/>}/>
+                 <Route path="/Loggedin" element={<App/>}/>
                 <Route path="sign-up" element={<Signup />} />
                 <Route path="sign-in" element={<Login />} />
             </Route>
--- Frontend/src/components/LandingHeader.jsx
+++ Frontend/src/components/LandingHeader.jsx
@@ -1,14 +1,16 @@
 
-function Header() {
+function LANDINGHeader() {
     
     return (
         <div className="flex bg-blue-800 h-24">
             <h1 className="flex-1 w-14 ml-4 text-3xl font-mono">MarketMate</h1>
             <input type="text" placeholder="Search Products, Brands, and Categories" className="flex-auto w-50 h-20 mt-2 bOrder-black" />
             <button className="flex-1 w-32 rounded shadow-gray-600  h-10 mt-8 ml-2" id='button'>Search</button>
+            <button className="flex-1 w-32 rounded shadow-gray-600  h-10 mt-8 ml-2" id='button'>Sign Up</button>
+            <button className="flex-1 w-32 rounded shadow-gray-600  h-10 mt-8 ml-2" id='button'>sign-in</button>
            
         </div>
     );
 }
 
-export default Header;
+export default LANDINGHeader;
